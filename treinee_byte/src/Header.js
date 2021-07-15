91258a28229d8548115cd6a28ef61f7f28db6f7b
Emanuel Oliveira <emanueldasilva011@gmail.com>
2021-07-14 23:58:11 -0300

    Add Header.css
    Adicionado Header.css e alterações no formato da página.

--- treinee_byte/src/Header.js
+++ treinee_byte/src/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Header.css';
+import img from './image-paisagem-720.png';
 
 function Header() {
     return (
-      <div className="App">
-        Header
+      <div className="Header">
+        <div class="textBoxHeader">
+          <h1>Header</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>  
+        <figure class="imagemHeader">
+          <img src={img} alt="Imagem Exemplo" width="auto" height="auto" />
+        </figure>
       </div>
     );
   }
